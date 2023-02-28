b5e81890c4f3bbe048273fa0c76e24387ea64943
NovikoffOleh <novikoff10000@gmail.com>
2023-02-28 19:40:05 +0200

    upd files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+function App() {
+    return <div className="wrapper">
+        <header>
+            <div className="headerLeft">
+                <svg/>
+            <div className="headerInfo">
+                <h3> Супер кросівки</h3>
+                <p> для стильних підлітків</p>
+            </div>
+            </div>
+            
+                <ul className="headerRight">
+                <li>
+                    <svg />
+                    <span>4000 грн </span>
+                </li>   
+                <li>
+                     <svg />
+                </li>     
+            </ul>
+        </header>
+        <div className="content">
+            <h1> Все що є</h1>
+            .....
+        </div>
+    </div>;
+}
+
+export default App;
